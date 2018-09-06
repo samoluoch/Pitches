0c9c98fe08a6cad9d4cd5139a8beaca4ec2d42d5
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-06 14:19:47 +0300

    defined test function and created the @manager.add_command decorator

---
+++ manage.py
@@ -0,0 +1,31 @@
+from app import create_app, db
+from flask_script import Manager, Shell, Server
+from app.models import User
+
+app = create_app('development')
+app = create_app('test')
+
+manager = Manager(app)
+manager.add_command('server', Server)
+
+
+@manager.add_command
+def test():
+    '''
+    Function that runs the unit test
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=3).run(tests)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User=User)
+
+
+
+
+
+if __name == '__main__':
+    manager.run()
