dff0a2c53e85b4f5dd1c73c3f3be56abf2a181e7
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-06 09:47:59 +0300

    imported flask, bootstrap, SQLAlchemy, and simpleMDE and initiated them in the __init__.py of the app root

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
+from flask_simplemde import SimpleMDE
+
+# Initialaizing Flask extensions
+bootstrap = Bootstrap
+db = SQLAlchemy
+mail = Mail()
+simple = SimpleMDE()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos', IMAGES)
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating app configurations
