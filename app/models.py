f9287736de2e940444e744a80cd18fe5ba1c7499
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-06 10:17:23 +0300

    created class Pitch in the models.py of the app and defined a function that instantiates the Pitch Object

--- app/models.py
+++ app/models.py
@@ -7,4 +7,14 @@ from datetime import datetime
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-    
+
+class Pitch:
+    '''
+    Class pitch that defines the structure of the Pitch Objcet
+    '''
+    def __init__(self, id, title, statement, vote_count):
+        self.id = id
+        self.title = title
+        self.statement = statement
+        self.vote_count = vote_count
+
