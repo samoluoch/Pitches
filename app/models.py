b39a1f20b0f6797fc5c17be298d19fbae5d31c11
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-06 10:34:45 +0300

    created a class User that defines the columns in the users database

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
+from . import db
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -18,3 +19,10 @@ class Pitch:
         self.statement = statement
         self.vote_count = vote_count
 
+class User(UserMixin, db.Model):
+    '''
+    Class User that defines the columns of the user user database in the database
+    '''
+    __tablename__ = 'users'
+    
+    id = db.Column
