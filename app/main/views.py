22925e3fb6fe4a0e08d65bc1e939ff4e95421afb
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-06 15:39:08 +0300

    defined a decorator that displays the details of the pitch

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,26 @@
 from flask import render_template
 from . import main
 from .. import db
+from ..models import Pitch
 
 # Views
 @main.route('/')
 def index():
     '''
-    View page function that returns the details on the index page
+    View page function that returns the pitch titles on the index page
     '''
 
     title = 'Welcome to the Pitch Page'
     return render_template('index.html', title=title)
 
+@main.route('/pitch/<int:id>')
+def pitch(id):
+    '''
+    View pitch function that returns the details of the pitch
+    '''
+    pitch = Pitch.get_pitches.get(id)
+    
+    return render_template('pitch.html', title=title, statement=statement, vote_count=vote_count)
 
 
 
