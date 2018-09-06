875153ce970ef9d3bdbc9a639ee8353f4298de11
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-06 21:06:45 +0300

    created a class for categories that will make a table to store the different pitch categories. Also defined a function that saves the diffeent categories to the database

--- app/models.py
+++ app/models.py
@@ -8,21 +8,6 @@ from . import db
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
-class New_Pitch:
-    '''
-    Class pitch that defines the structure of the Pitch Objcet
-    '''
-    def __init__(self, id, title, statement, vote_count):
-        self.id = id
-        self.title = title
-        self.statement = statement
-        self.vote_count = vote_count
-
-    def save_pitch(self):
-        db.session.add(self)
-        db.session.commit()
-
         
 
 class User(UserMixin,db.Model):
@@ -55,4 +40,33 @@ class Pitch(db.Model):
         db.session.commit()
 
 
+class Category(db.Model):
+    '''
+    Class that defines a table for the different pitch categories
+    '''
+    __tablename__ = 'category'
 
+    id = db.Column(db.Integer,primary_key = True)
+    details = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+    pitch = db.relationship("Pitch", backref = "category", lazy="dynamic")
+
+    def save_category(self):
+        db.session.add()
+        db.session.commit()
+
+
+
+# class New_Pitch:
+#     '''
+#     Class pitch that defines the structure of the Pitch Objcet
+#     '''
+#     def __init__(self, id, title, statement, vote_count):
+#         self.id = id
+#         self.title = title
+#         self.statement = statement
+#         self.vote_count = vote_count
+
+#     def save_pitch(self):
+#         db.session.add(self)
+#         db.session.commit()
