fcf25605b6518e61fe7bf187f3ee2a76ab2a128c
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-06 16:07:27 +0300

    added Pitch class that defines the tables in the db Model and the structure of the Pitch

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class Pitch:
+class New_Pitch:
     '''
     Class pitch that defines the structure of the Pitch Objcet
     '''
@@ -22,7 +22,7 @@ class Pitch:
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     '''
-    Class User that defines the columns of the user user database in the database
+    Class User that defines the tables of the user user database in the database
     '''
     
     id = db.Column(db.Integer,primary_key = True)
@@ -31,3 +31,18 @@ class User(UserMixin,db.Model):
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(255))
     reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
+
+class Pitch(db.Model):
+    '''
+    Class pitch that defines the tables in the pitch database
+    '''
+    __tablename__ = 'pitch'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(255), index = True)
+    statement = db.Column(db.String(255))
+    vote_count = db.Column(db.String)
+    date_created = db.Column(db.Date, default=datetime.now)
+
+
+
