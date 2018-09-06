07e26db0075364b162931a9f2052b73c0b6829e6
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-06 11:01:28 +0300

    added more columns in the users database. Created parent confiuration class and defined the mail server, mail port, mail username, and password

--- app/models.py
+++ app/models.py
@@ -25,4 +25,9 @@ class User(UserMixin, db.Model):
     '''
     __tablename__ = 'users'
     
-    id = db.Column
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255), index = True)
+    email = db.Column(db.String(255), unique = True, index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(255))
+    reviews = db.relationship('Review',backref = 'user',lazy = "dynamic")
---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    '''
+    This is the general configuration parent class
+    '''
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
