2a778ec3eb1d00d9bffba3389843741d6d047fa0
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-07 16:35:26 +0300

    extended macros to the index.html

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,7 +30,8 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        u
+        ser = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
