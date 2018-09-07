38a99d27d124867b43dc8bd94b699d5dd61e25f8
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-07 14:56:53 +0300

    created a function that queries the database and returns the list of pitches

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template
 from . import main
-# from .. import db
-# from ..models import Pitch
+from .. import db
+from ..models import Pitch
 
 # Views
 @main.route('/')
@@ -9,20 +9,31 @@ def index():
     '''
     View page function that returns the pitch titles on the index page
     '''
+    # interviews = Pitch.query.get('interviews)
+    # inspirational = get_pitch('inspirational')
+    # general = get_pitch('general')
+
+
 
     title = 'Welcome to the Pitch Page'
     return render_template('index.html', title=title)
 
-# @main.route('/pitch/<int:id>')
-# def pitch(id):
-#     '''
-#     View pitch function that returns the details of the pitch
-#     '''
-#     # pitch = Pitch.get_pitches.get(id)
+@main.route('/pitch/<int:id>')
+def pitch(id):
+    '''
+    View pitch function that returns the details of the pitch as well as enable the reation of a comment
+    '''
+    pitch = Pitch.query.get(id)
 
-#     title = 'Welcome to the Pitch Page'
+    title = 'Welcome to the Pitch Page'
+
+    if pitch is None:
+        abort(404)
+
+    comment =  Comments.get_comments(id)
     
-#     return render_template('pitch.html', title=title)
+    return render_template('pitch.html', title=title)
+
 
 
 
