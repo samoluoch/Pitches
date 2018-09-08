ca2b3a8eb5ac9955b1a4e723f889e71afc8aca3d
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-08 13:27:44 +0300

    added the registration form and rendered it to the navbar

--- app/auth/views.py
+++ app/auth/views.py
@@ -16,7 +16,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "Pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/logout')
@@ -30,8 +30,7 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        u
-        ser = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
         db.session.add(user)
         db.session.commit()
         return redirect(url_for('auth.login'))
