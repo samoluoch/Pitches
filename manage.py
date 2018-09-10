39d1925620960b7fdcda33232752c2b029d86ccc
samsoluoch@gmail.com <samsoluoch@gmail.com>
2018-09-10 17:01:13 +0300

    Deployment attemp 151. Changed config to production.

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app,db
 from flask_script import Manager, Shell, Server
-from app.models import User
+from app.models import User, Pitch, Comment
 from flask_migrate import Migrate, MigrateCommand
 # app = create_app('development')
 app = create_app('production')
@@ -23,7 +23,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db, User=User, Pitch=Pitch, Comment=Comment)
 
 
 
